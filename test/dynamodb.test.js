d40cf87f29e18e55f2a029a2ab8e9a26584383da
Peter Elger <elger.peter@gmail.com>
2013-07-30 12:43:27 +0100

    refactored for seneca 0.5.9

--- test/dynamodb.test.js
+++ test/dynamodb.test.js
@@ -1,35 +1,36 @@
+/*jslint node: true */
+/*global describe:true, it:true, mocha:true*/
 /* Copyright (c) 2012 Seamus D'Arcy */
+/* These tests assume the existance of tables 'foo' and 'moon_bar' with hash key 'id' 
+ *
+ * Run with
+ * mocha  dynamodb.test.js --globals encoding
+ */
 
-// These tests assume the existance of tables 'foo' and 'moon_bar' with hash key 'id'.
+"use strict";
 
-var seneca   = require('seneca');
-var shared   = require('seneca/test/store/shared');
+var seneca = require('seneca');
+var shared = require('seneca/test/store/shared');
+var keys = require('./keys.mine.js');
+var si = seneca();
 
-var config = {
-  log:'print'
-};
-
-var si = seneca(config);
-
-var senecaDynamoDBStore = require('../lib/dynamodb-store');
-var senecaDynamoDBStoreOpts = {
-  accessKeyId: 'ACCESSKEYID',
-  secretAccessKey: 'SECRETACCESSKEY',
-  endpoint: 'ENDPOINT'
-};
-
-si.use(senecaDynamoDBStore, senecaDynamoDBStoreOpts);
+si.use(require('..'), {keyid: keys.id, secret: keys.secret, endpoint: keys.endpoint});
 
 si.__testcount = 0;
 var testcount = 0;
 
-module.exports = {
-  basictest: (testcount++, shared.basictest(si)),
-  extratest: (testcount++, extratest(si)),
-  closetest: shared.closetest(si, testcount)
-};
 
-function extratest(si) {
-  console.log('EXTRA');
-  si.__testcount++;
-}
+describe('dynamodb', function(){
+  it('basic', function(done){
+    this.timeout(0);
+    testcount++;
+    shared.basictest(si, done);
+  });
+
+  it('close', function(done){
+    this.timeout(0);
+    shared.closetest(si, testcount, done);
+  });
+});
+
+
---
+++ test/keys.js
@@ -0,0 +1,5 @@
+// copy this file to ./keys.mine.js and replace the id and secret
+exports.id = 'YOUR_ID';
+exports.secret = 'YOUR_KEY';
+exports.endpoint = 'YOUR_ENDPOINT';
+
