a0e9734afae8f22951a9d0eea57f818631e9dade
Vito Tardia <vito@tardia.me>
2015-09-18 18:06:30 +0200

    Added region parameter to test conf

--- test/keys.js
+++ test/keys.js
@@ -1,5 +1,6 @@
-// copy this file to ./keys.mine.js and replace the id and secret
+// Copy this file to ./keys.mine.js and replace the id and secret
+// For Local DynamoDB define endpoint will be "http://localhost:8000"
 exports.id = 'YOUR_ID';
 exports.secret = 'YOUR_KEY';
 exports.endpoint = 'YOUR_ENDPOINT';
-
+exports.region = "eu-west-1";
